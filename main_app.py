59bcbe724f6ea1e2f4c3a215e10351967314c583
Tomáš Roch <rochtomass@gmail.com>
2023-12-18 18:51:21 +0100

    Add example app

--- main_app.py
+++ main_app.py
@@ -1,26 +1,46 @@
+import os
+import cv2
 from ultralytics import YOLO
 import supervision as sv
+from PIL import Image
+
+
 
 BLUR_LEVEL = 200
 
-model = YOLO('./runs/detect/train5/weights/best.pt')  # load a custom model
-results = model.predict(source='images/val/erotic-pink-nude-7751268.jpeg', save=False, save_txt=False) # base use of model
+model = YOLO('./runs/detect/google2/weights/best.pt')  # load a custom model
 
-### Blur detections part
-detections = sv.Detections.from_ultralytics(results[0])
-image = results[0].orig_img
+directory = './images/test'
+for filename in os.listdir(directory):
+    if(filename.startswith('.')): #skip hidden file
+        continue
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        results = model.predict(source=f, save=False, save_txt=False)  # base use of model
 
-blur_annotator = sv.BlurAnnotator(BLUR_LEVEL)
+        ### Blur detections part
+        detections = sv.Detections.from_ultralytics(results[0])
+        image = results[0].orig_img
 
-annotated_frame = blur_annotator.annotate(
-    scene=image.copy(),
-    detections=detections
-)
-sv.plot_image(image=annotated_frame, size=(16, 16))
+        blur_annotator = sv.BlurAnnotator(BLUR_LEVEL)
+
+        annotated_frame = blur_annotator.annotate(
+            scene=image.copy(),
+            detections=detections
+        )
+        #show image directily
+        #sv.plot_image(image=annotated_frame, size=(16, 16))
+
+        # save a image using extension
+        im = Image.fromarray(annotated_frame)
+        b, g, r = im.split()
+        im = Image.merge("RGB", (r, g, b))
+        #im.show()
+        im.save(directory + "/processed/" + filename)
 
 
-
-
+print('end file')
 
 
 
--- train.py
+++ train.py
@@ -3,5 +3,6 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model
 
 #MPS umožní využít maximální potenciál M1,M2,M3... chipů
-results = model.train(data='config.yaml', epochs=20, device='mps')  #imgsz=640
+
+results = model.train(data='config.yaml', epochs=300, device='mps')
 
